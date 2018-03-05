af17355f05098915d61430fb3fdfa03f0974b383
dhelper <dror.helper@gmail.com>
2018-03-05 22:06:44 +0200

    Created simple service

---
+++ MicroServiceDemo/LunchService/Controllers/LunchController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LunchService.Controllers
+{
+    [Route("api/[controller]")]
+    public class LunchesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Friend> Get()
+        {
+            return new[] {new Friend {Name = "Jasse"}, new Friend {Name = "Dave"}};
+        }
+    }
+
+    public class Friend
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ MicroServiceDemo/LunchService/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LunchService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
