a53300ca8622a250e3b2a1b46378fff410193163
dhelper <dror.helper@gmail.com>
2018-03-05 22:21:01 +0200

    Added To Functions

--- MicroServiceDemo/LunchService/Controllers/LunchController.cs
+++ MicroServiceDemo/LunchService/Controllers/LunchController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LunchService.Controllers
@@ -7,14 +8,37 @@ namespace LunchService.Controllers
     public class LunchesController : Controller
     {
         [HttpGet]
-        public IEnumerable<Friend> Get()
+        public IEnumerable<Friend> GetMyLunchPlans()
         {
             return new[] {new Friend {Name = "Jasse"}, new Friend {Name = "Dave"}};
         }
+
+        [HttpGet("Free/{city}")]
+        public IEnumerable<Friend> GetAvailableFriendsByCity([FromQuery] string city)
+        {
+            return new[] {new Friend {Name = "Jasse"}, new Friend {Name = "Dave"}};
+        }
+
+        [HttpPost()]
+        public void AskFriendsToJoinByLocation([FromBody]string location)
+        {
+            // get all in same location
+            // create new lunch for date
+        }
+    }
+
+  
+
+    public class Lunch
+    {
+        public int Id { get; set; }
+        public IEnumerable<Friend> Friends { get; set; }
+        public string Location { get; set; }
     }
 
     public class Friend
     {
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
